80debcb6af3dbdb57419625c39f7b8b66b71a97b
dingxiong <dingxiong203@gmail.com>
2015-12-24 16:42:02 +0800

    ignore

--- test3.m
+++ test3.m
@@ -1,23 +1,30 @@
 % test eigenvalues
 global A;
 
-N = 40;
+N = 100;
 k = 5;
 m = 10;
 
 A = rand(N, N);
-v1 = rand(N, 1);
-[e, v, isC, flag, nc, ni] = KrylovSchurEig(@Ax, v1, N, k, m, 100, 2e-16);
-disp(['error of KrylovSchurEig : ', num2str(norm(A * v - v * diag(e)))]);
 
 [ev, es] = eig(A); 
 disp(['error of eig(A) : ', num2str(norm(A * ev - ev * es))]);
 es = diag(es);
 [~, ix] = sort(abs(es), 'descend');
 es = es(ix);
-
-disp('number of converged eigenvalues: '); disp(nc);
-disp('number of iteration used ');   disp(ni);
-disp('eigenvalues by KrylovSchurEig '); disp(e);
 disp('eigenvalues by matlab command eig() '); disp(es(1:k));
 
+for i = 1:3
+    fprintf(1, ' =======  trial i = %d  ========== \n', i);
+    
+    v1 = rand(N, 1);
+    [e, v, isC, flag, nc, ni] = KrylovSchurEig(@Ax, v1, N, k, m, 200, 2e-16);
+    disp(['error of KrylovSchurEig : ', num2str(norm(A * v - v * diag(e)))]);
+    [~, ix] = sort(abs(e), 'descend');
+    e = e(ix);
+    
+    fprintf(1, 'number of converged eigenvalues: %d \n', nc);
+    fprintf(1, 'number of iteration used : %d \n', ni);
+    disp('eigenvalues by KrylovSchurEig '); disp(e);
+    
+end
